9143c778c4d9658fe2aa3decb76334adfbab5838
Andriy V. Koval <koval.andrey@gmail.com>
2019-05-16 10:53:33 -0700

    Update gls-activity.R
    #1

--- analysis/gls-activity/gls-activity.R
+++ analysis/gls-activity/gls-activity.R
@@ -20,7 +20,7 @@ library(ggpubr)   # documents
   
 # ---- declare-globals ---------------------------------------------------------
 path_file_input <- "./data-unshared/derived/0-greeted-gls.rds"
-
+html_flip <- FALSE
 # ---- load-data ---------------------------------------------------------------
 dto      <- readRDS(path_file_input)
 dto %>% pryr::object_size(); dto %>% class(); dto %>% names()
@@ -54,8 +54,42 @@ get_a_sample <- function(
 
 # ---- tweak-data ---------------------------------------------------------------
 
+# create auxilary variables
+ds <- ds %>% 
+  dplyr::mutate(
+    year = lubridate::year(date)
+    ,month = lubridate::month(date)
+    ,weekday = lubridate::wday(date)
+  )
+ds %>% glimpse()
+
+# ----- basic-questions -------------------------------------------------
+
+#How many counties and zipcodes were engaged by the program?
+#How many distincts training types?
+#How many individuals received training?
+ds %>% 
+  dplyr::group_by(region) %>% 
+  dplyr::summarize(
+    n_counties = length(unique(county))
+    ,n_zipcodes = length(unique(zipcode))
+    ,n_training_types = length(unique(type_training))
+    ,total_persons_trained = sum(na.omit(n_trained))
+    )
+
+ds %>% 
+  dplyr::group_by(region, county,zipcode) %>% 
+  dplyr::summarize(
+     n_zipcodes = length(unique(zipcode))
+    ,n_program_types = length(unique(type_training))
+    ,total_persons_trained = sum(n_trained)
+    ) %>% 
+  neat()
+
 # ---- basic-graph -------------------------------------------------------
 
+g1 <- ds %>% 
+  ggplot2::ggplot(aes(x = date, y = ))
 
 # ---- define-utility-functions ---------------
 
