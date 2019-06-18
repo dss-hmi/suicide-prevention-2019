e781534c8e406a59351902713b06fda1e271b75d
Andriy V. Koval <koval.andrey@gmail.com>
2019-06-18 14:26:48 -0700

    Create gls-activity-graph-drafts.R
    @jodh , please study g2 and wrap up its production

---
+++ analysis/gls-activity/gls-activity-graph-drafts.R
@@ -0,0 +1,179 @@
+# Lines before the first chunk are invisible to Rmd/Rnw callers
+# Run to stitch a tech report of this script (used only in RStudio)
+knitr::stitch_rmd(script = "./manipulation/0-greeter.R", output = "./stitched-output/manipulation/0-greeter.md", figure)
+# knitr::stitch_rmd(script = "./manipulation/0-greeter.R", output = "./manipulation/stitched-output/0-greeter.md", )
+# this command is typically executed by the ./manipulation/governor.R
+
+rm(list=ls(all=TRUE)) #Clear the memory of variables from previous run. 
+# This is not called by knitr, because it's above the first chunk.
+cat("\f") # clear console when working in RStudio
+
+# ---- load-sources ------------------------------------------------------------
+# Call `base::source()` on any repo file that defines functions needed below. 
+base::source("./scripts/common-functions.R")
+
+# ---- load-packages -----------------------------------------------------------
+# Attach these packages so their functions don't need to be qualified
+library(magrittr) # pipes
+library(dplyr)    # disable when temp lines are removed
+library(ggplot2)  # graphs
+library(ggpubr)   # documents
+  
+# ---- declare-globals ---------------------------------------------------------
+path_file_input <- "./data-unshared/derived/0-greeted-gls.rds"
+html_flip <- FALSE
+baseSize <- 10
+# ---- load-data ---------------------------------------------------------------
+dto      <- readRDS(path_file_input)
+dto %>% pryr::object_size(); dto %>% class(); dto %>% names()
+
+# assign aliases for this report
+ds <- dto
+
+# ----- custom-functions --------------------------------------
+get_a_sample <- function(
+  d,
+  varname            # unique of these
+  ,sample_size
+  ,show_all = FALSE
+){
+  # varname = "offense_arrest_cd"
+  sample_pool <- ds %>% 
+    dplyr::distinct_(.dots = varname) %>% na.omit() %>% 
+    as.list() %>% unlist() %>% as.vector() 
+  if(show_all){ sample_size = length(sample_pool)}
+  selected_sample <- sample_pool %>% sample(size = sample_size, replace = FALSE )
+  
+  return(selected_sample)
+}  
+# How to use
+# ds %>% get_a_sample("person_id",  5)
+# ds %>% get_a_sample("offense_arrest_cd",  5, show_all = T) 
+# set.seed(42)
+# target_sample <- ds %>% 
+#   dplyr::filter(n_offenses > 1L) %>% 
+#   get_a_sample("person_id", 500)
+ds %>% distinct(region)
+# ---- tweak-data ---------------------------------------------------------------
+
+
+# ---- graph-2 ---------------------------------
+ds_plot_2 <- ds %>% 
+  # dplyr::filter(audience=="professionals") %>% 
+  dplyr::filter(audience=="community") %>% 
+  dplyr::mutate(
+    year     = lubridate::year(date)
+    ,month   = lubridate::month(date)
+    ,weekday = lubridate::wday(date)
+    ,rgn     = car::recode(
+      region,
+      "
+      'central'='CN'
+      ;'southeast'='SE'
+      ;'northeast'='NE  '
+      "
+    )
+    ,month = ifelse( month<=9, paste0("0",month), month)
+    ,year_month = paste0(substr(year,3,4),"-",month)
+    ,county  = paste0(county,"-",rgn)
+    ) %>% 
+  dplyr::group_by(county, year, month) %>% 
+  dplyr::mutate(
+    n_trained_sum = sum(n_trained)
+  ) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::arrange(county, year, month) %>% 
+  dplyr::mutate(
+    value_ntile_interval = Hmisc::cut2(n_trained_sum,g = 5 )
+    ,value_ntile_interval = factor(value_ntile_interval, levels = rev(levels(value_ntile_interval)))
+  )
+ds_plot_2 %>% glimpse()
+
+cr_levels <- ds_plot_2 %>% 
+  dplyr::arrange(region, county) %>% 
+  dplyr::distinct(county ) %>% 
+  as.list() %>% unlist() %>% as.character()
+
+ds_plot_2 <- ds_plot_2 %>% 
+  dplyr::mutate(
+    county  = factor(county, levels = cr_levels)
+    ,county = factor(county, levels = rev(levels(county)))
+  )
+
+g2 <- ds_plot_2 %>% 
+  # ggplot2::ggplot(aes(x = date, y = county))+
+  ggplot2::ggplot(aes(x = year_month, y = county))+
+  # geom_tile(aes(fill = n_trained))+
+  geom_raster(aes(fill = value_ntile_interval))+
+  # geom_tile(aes(fill = value_ntile_interval))+
+  # geom_text(aes(label = n_trained_sum))+
+  # facet_grid(region ~ .)+
+  # scale_x_date(labels = date_format("%m-%y"), breaks = date_breaks("2 month") )+
+  theme_minimal()+
+  # scale_fill_viridis_d(end=.7)+
+  scale_fill_viridis_d(end=.85, option = "plasma")+
+  # scale_fill_viridis_d(end=.8, option = "magma")+
+  # scale_fill_viridis_d(end=.9, option = "inferno")+
+  # scale_fill_viridis_c()+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+g2
+
+
+# ---- graph-3 ---------------------------------
+# grid of regions, X= date, Y=n_trained, professional audience
+library(scales)
+
+# not sure what to do with the graph, come back later
+
+ds_plot_3 <- ds %>% 
+  dplyr::filter(audience=="professionals") %>% 
+  # dplyr::filter(county %in% c("Volusia","Lake","Palm Beach") ) %>%
+  # dplyr::filter(!county == "Orange") %>%
+  # dplyr::filter(county == "St Lucie") %>%
+  dplyr::arrange(date) %>% 
+  dplyr::group_by(county) %>% 
+  dplyr::mutate(
+    cum_n_trained = cumsum(n_trained)
+  ) %>% 
+  dplyr::ungroup()
+
+g3 <- ds_plot_3 %>% 
+  # ggplot2::ggplot(aes(x = date, y = n_trained, fill = county))+
+  # ggplot2::ggplot(aes(x = date, y = cum_n_trained, fill = county))+
+  ggplot2::ggplot(aes(x = date, y = cum_n_trained, color = region))+
+  # geom_bar(stat="identity") +
+  # geom_area(alpha = .5, position = "stack")+
+    # geom_area(aes(group = county), alpha = .5)+
+  geom_line(aes(group = county), size = 2.5, alpha = .5)+
+  geom_line(aes(group = county), color = "black", size = .2, alpha = .5)+
+  facet_grid(region ~ . , scales = "free") +
+  # facet_wrap("county") +
+  scale_x_date(
+    labels  = scales::date_format("%m-%Y")
+    ,breaks = scales::date_breaks("6 month")
+  )+
+  theme_minimal()
+g3
+
+# ---- basic-graph -------------------------------------------------------
+
+
+# ---- define-utility-functions ---------------
+
+# ---- save-to-disk ----------------------------
+
+# ---- publish ---------------------------------
+rmarkdown::render(
+  # input = "./analysis/gls-activity/gls-activity-1.Rmd"
+  input = "./analysis/gls-activity/gls-activity-2-coverage.Rmd"
+  ,output_format = c(
+    "html_document" 
+    # "pdf_document"
+    # ,"md_document"
+    # "word_document" 
+  )
+  ,clean=TRUE
+)
+
+
+
