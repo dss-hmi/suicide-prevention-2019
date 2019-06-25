503679ccfe96924baada0032c9071abb40c27ad9
Andriy V. Koval <koval.andrey@gmail.com>
2019-06-25 08:51:08 -0700

    developed g4

--- analysis/eda-combined/eda-combined.R
+++ analysis/eda-combined/eda-combined.R
@@ -82,22 +82,77 @@ ds %>% explore::describe_all()
 # the most granular level includes (6):
 # county, year, sex, age_group, race, ethnicity
 d1 <- ds %>% 
+  # apply filters to better understand the structure of the data
+  # dplyr::filter(county     %in% c("Orange") ) %>%
+  # dplyr::filter(year       %in% c("2015")   ) %>%
+  # dplyr::filter(sex        %in% c("Male")   ) %>%
+  # dplyr::filter(age_group  %in% c("15_19")  ) %>%
+  # dplyr::filter(race       %in% c("White")  ) %>%
+  # dplyr::filter(ethnicity %in% c("Non-Hispanic") )%>%
+  # dplyr::filter(ethnicity %in% c("Hispanic","Non-Hispanic") )%>%
   dplyr::group_by(county, year, sex, age_group, race, ethnicity) %>% # no aggregation
-  # dplyr::group_by(county, year) %>%
-  # dplyr::group_by(county, year, sex,) %>%
-  # dplyr::group_by(county, year, sex, age_group) %>%
-  # dplyr::group_by(county, year, sex, ethnic) %>%
-  # dplyr::group_by(county, year,sex,race,ethnicity,age_group) %>%
-  dplyr::summarize(
+  # to exemplify useful aggregates:
+  # dplyr::group_by(county, year                               ) %>%
+  # dplyr::group_by(county, year, sex                          ) %>%
+  # dplyr::group_by(county, year, sex, age_group               ) %>%
+  # dplyr::group_by(county, year, sex, ethnic                  ) %>%
+dplyr::summarize(
     population_count   = sum(population_count,   na.rm = T)
     ,deaths_by_suicide = sum(deaths_by_suicide,  na.rm = T)
     ,professionals     = sum(professionals,      na.rm = T)
     ,community         = sum(community,          na.rm = T)
   ) 
-
+# use the code for preparing data for custom graphs
 d1 %>% glimpse(60)
 
 d1 %>% explore::describe()
+
+# ---- explore-4 -------------------------------
+# to understand how suicide rate varies across time and counties for youth regardless of race
+d4 <- ds %>% 
+  dplyr::filter(year %in% c(2006:2017)) %>% 
+  # dplyr::filter(age_group %in% c("10_14","15_19","20_24")) %>%
+  dplyr::filter(age_group %in% c("20_24")) %>%
+  # dplyr::filter(age_group %in% c("15_19")) %>%
+  # dplyr::filter(age_group %in% c("10_14")) %>% 
+  dplyr::group_by(county, year, sex) %>%
+  dplyr::summarize(
+    population_count   = sum(population_count,   na.rm = T)
+    ,deaths_by_suicide = sum(deaths_by_suicide,  na.rm = T)
+    ,suicide_rate_per100k = (deaths_by_suicide / population_count) *100000
+    ,professionals     = sum(professionals,      na.rm = T)
+    ,community         = sum(community,          na.rm = T)
+  ) %>% 
+  dplyr::ungroup() %>%
+  dplyr::mutate(
+    # produces the same results as within group_by() summarization
+    # suicide_rate_per100k = (deaths_by_suicide / population_count) *100000
+    years_since_2000 = as.integer(as.integer(year) - 2000)
+    # ,sex = factor(sex, levels = c("Male","Female") )
+    ,sex = factor(sex, levels = c("Female","Male") )
+  ) 
+d4 %>% explore::describe()
+g4 <- d4 %>% 
+  ggplot(aes(
+    x     = years_since_2000
+    ,y    = suicide_rate_per100k
+    # ,y    = deaths_by_suicide
+    # ,y    = population_count
+    ,fill = sex
+  ))+
+  geom_bar(stat = "identity", position = "stack", color = "black")+
+  # geom_point()+
+  # geom_line(aes(group = county))+
+  facet_wrap("county")+
+  scale_x_continuous(breaks = seq(6,17,2))+
+  scale_fill_viridis_d(end = .9, option = "plasma")+
+  # scale_fill_viridis_d(end = .5, option = "magma")+
+  # scale_fill_viridis_d(end = .5, option = "inferno")+
+  # scale_fill_viridis_d(end = .5, option = "viridis")+
+  # scale_fill_manual(values = c("Female"="darksalmon","Male" = "deepskyblue1"))+
+  # theme_minimal()
+  theme_bw()
+g4
 
 # ---- explore-1 -------------------------------
 # g1 <- ds %>%
@@ -194,7 +249,7 @@ g3 <- ds %>%
   # scale_fill_manual(values = c("central"="red","northeast"="blue","southeast"="green","NA"="NA"))+
   theme_minimal()
 g3
-# ---- explore-4 -------------------------------
+
 
 
 # ---- save-to-disk ----------------------------
