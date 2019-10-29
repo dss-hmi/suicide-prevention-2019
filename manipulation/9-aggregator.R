201345896a7405d7b107927f8f350fcd20324890
Andriy V. Koval <koval.andrey@gmail.com>
2019-10-29 08:31:56 -0400

    function to compute rate stable

--- manipulation/9-aggregator.R
+++ manipulation/9-aggregator.R
@@ -87,179 +87,57 @@ ds_population_suicide <- dplyr::left_join(
   ) 
 ds_population_suicide %>% dplyr::glimpse()
 
+# function-to-aggregate ---------------------------------------------------
 
-
-
-
-
-
-
-
-
-
-
-ds_gls_by_year %>% dplyr::glimpse()
-# note that `audience` and `type_training` are more granular than county-by-year
-ds_population_by_year <- ds_population %>% 
-  dplyr::rename(
-    "population_count" = "count" # to be more explicit and because a better label
-  )
-ds_suicide_by_year <- ds_suicide %>% 
-  dplyr::group_by(county, year, sex, race, ethnicity, age_group, mortality_cause) %>% 
-  dplyr::summarize(
-    n_suicides = sum(n_suicides, na.rm = T)
-  )
-ds_suicide_by_year %>% dplyr::glimpse()
-# note that `mortality_cause` is more granular than county-by-year
-# to help with elementwise list operations:
-ls_ds <- list(
-  "population" = ds_population_by_year # first, because most complete
-  ,"suicide"   = ds_suicide_by_year
-  ,"gls"       = ds_gls_by_year # last, because left join
-)
-ls_ds %>% lapply(dplyr::glimpse,100) %>% invisible() 
-
-# ---- aggregate-1-suicide ------------------------------
-#let us aggregate at the level of `population`
-# this means that we will provide a single measure at the resolution:
-# county - year - sex - age_group - race - enthicity 
-# for every unique combination of values on these six we want:
-# `population_count` - number of people alive 
-# `resident_deaths`  - number of registered deaths among residents
-# `community`        - number of people reached through community event
-# `professionals`    - number of professionals who recieved training
-
-
-ls_ds_1 <- list(
-  "population" = ls_ds[["population"]] 
-  ,"suicide" = ls_ds[["suicide"]]  %>% 
-    dplyr::group_by(county, year, sex, race, ethnicity, age_group) %>% 
-    # aggregating over `mortality_casue`
+compute_rate <- function(
+  d,
+  grouping_frame
+){
+  d <- ds_population_suicide
+  grouping_frame <- c("county","year")
+  
+  d1 <- d %>%
+    dplyr::group_by_(.dots = grouping_frame) %>%
     dplyr::summarize(
-      resident_deaths = sum(resident_deaths, na.rm = T)
+      n_population = sum(n_population, na.rm = T)
+      ,n_suicides = sum(n_suicides, na.rm = T)
+    ) %>% 
+    dplyr::ungroup() %>% 
+    dplyr::mutate(
+      suicide_rate_per_100k  = (n_suicides/n_population)*100000
     )
-  ,"suicide_wide" = ls_ds[["suicide"]] %>% 
-    tidyr::spread(mortality_cause, resident_deaths)
+  return(d1)
+}
+# how to use
+d_computed <- ds_population_suicide %>% compute_rate(grouping_frame = c("county","year"))
+d_computed %>% arrange(desc(suicide_rate_per_100k))
+
+
+# apply rate computation --------------------------------------------------
+
+grouping_frame <- c("county","year")
+path_to_folder <- "./data-unshared/derived/rate/"
+
+
+d_computed <- ds_population_suicide %>% compute_rate(grouping_frame = grouping_frame)
+
+file_name <- paste0(path_to_folder, paste0(grouping_frame,collapse = "-"),".csv")
+readr::write_csv(d_computed, file_name)
+
+
+# create loop to apply the function
+
+ls_grouping_frame <- list(
+  c("county")
+  ,c("county","year")
+  ,c("county","year","sex")
 )
-ls_ds_1 %>% lapply(dplyr::glimpse,100) %>% invisible() 
-ds_aggregate_1 <- ls_ds_1 %>% Reduce(function(a , b) dplyr::left_join( a, b ), . ) 
-ds_aggregate_1 %>% dplyr::glimpse(100)
-
-# ----- tweak-aggregate-1 ------------------------------------------
-
-ds_aggregate_1 <- ds_aggregate_1 %>% 
-  dplyr::rename(
-    "deaths_by_suicide" = "resident_deaths" # to remind what we count
-  ) %>% 
-  dplyr::mutate(
-    # to have a single variable describing racial background
-    race_ethnicity = paste0(race," + ", ethnicity),
-    deaths_per_100k  = (deaths_by_suicide/population_count)*100000
-    # to aid in graph production ( note the spaces at the end of "NE  ")
-  ) %>% 
+for (frame_i in ls_grouping_frame){
+  # frame_i %>% print()
+  file_name_i  <- paste0(path_to_folder, paste0(frame_i,collapse = "-"),".csv")
+  d_computed <- ds_population_suicide %>% compute_rate(grouping_frame = frame_i)
+  d_computed %>% readr::write_csv(file_name_i)
   
-  dplyr::select(county, year, sex, age_group, race, ethnicity, race_ethnicity, # context
-                population_count, deaths_by_suicide, #measures
-                dplyr::everything()
-  ) 
-ds_aggregate_1 %>% explore::describe_all()
-
-# ---- test-snippets -------------------------------
-# ds_aggregate_1 %>% 
-#   dplyr::filter(county == "Brevard") %>% 
-#   dplyr::summarize(
-#     pop = sum(community,na.rm = T)
-#   )
+}
 
 
-# mc <- ds_suicide %>% 
-#   dplyr::distinct(county, year, sex, age_group, race, ethnicity, resident_deaths, mortality_cause) %>% 
-#   dplyr::rename(
-#     "deaths_by_suicide" = "resident_deaths" # to remind what we count
-#   )
-# mc %>% explore::describe_all()
-# d <- dplyr::left_join(
-#   ds_aggregate_1
-#   ,mc
-# )
-
-# ----- save-to-disk ---------------------------------------------------------------
-
-ds_gls                   %>% readr::write_csv("./data-public/derived/gls.csv")
-ds_population            %>% readr::write_csv("./data-public/derived/population.csv")
-ds_suicide               %>% readr::write_csv("./data-public/derived/suicide.csv")
-ds_aggregate_1          %>% readr::write_csv("./data-public/derived/aggregate-1-suicide.csv")
-
-
-# ---- aggregate-2-gls ------------------------------
-#let us aggregate at the level of `population`
-# this means that we will provide a single measure at the resolution:
-# county - year - sex - age_group - race - enthicity 
-# for every unique combination of values on these six we want:
-# `population_count` - number of people alive 
-# `resident_deaths`  - number of registered deaths among residents
-# `community`        - number of people reached through community event
-# `professionals`    - number of professionals who recieved training
-
-
-ls_ds_2 <- list(
-  "population" = ls_ds[["population"]] 
-  ,"suicide" = ls_ds[["suicide"]]  %>% 
-    dplyr::group_by(county, year, sex, race, ethnicity, age_group ) %>% 
-    # aggregating over `mortality_casue`
-    dplyr::summarize(
-      resident_deaths = sum(resident_deaths, na.rm = T)
-    )
-  ,"suicide_wide" = ls_ds[["suicide"]] %>% 
-    # dplyr::mutate(
-    #   lethal_means = mortality_cause
-    # ) %>%
-    # tidyr::spread(lethal_means, resident_deaths)
-    tidyr::spread(mortality_cause, resident_deaths)
-  
-  ,"gls" = ls_ds[["gls"]]  %>%
-    dplyr::group_by(region, county, year, audience) %>%
-    # aggregating over `type_training`
-    dplyr::summarize(
-      n_trained = sum(n_trained, na.rm = T)
-    ) %>%
-    tidyr::spread(audience,n_trained)
-)
-ls_ds_2 %>% lapply(dplyr::glimpse,100) %>% invisible() 
-ds_aggregate_2 <- ls_ds_2 %>% Reduce(function(a , b) dplyr::left_join( a, b ), . ) 
-ds_aggregate_2 %>% dplyr::glimpse(50)
-
-# ----- tweak-aggregate-2 ------------------------------------------
-
-ds_aggregate_2 <- ds_aggregate_2 %>% 
-  dplyr::rename(
-    "deaths_by_suicide" = "resident_deaths" # to remind what we count
-  ) %>% 
-  dplyr::mutate(
-    # to have a single variable describing racial background
-    race_ethnicity = paste0(race," + ", ethnicity),
-    deaths_per_100k  = (deaths_by_suicide/population_count)*100000
-    # to aid in graph production ( note the spaces at the end of "NE  ")
-  ) %>% 
-  
-  dplyr::select(county, year, sex, age_group, race, ethnicity, race_ethnicity, # context
-                population_count, deaths_by_suicide, #measures
-                # mortality_cause,
-                dplyr::everything()
-  ) 
-ds_aggregate_2 %>% explore::describe_all()
-
-
-# ---- test-snippet-2 --------------------------
-# d1 <- ds_aggregate_2 %>% 
-#   dplyr::filter(county == "Orange") %>% 
-#   dplyr::filter(sex == "Male") %>% 
-#   dplyr::filter(year == "2016") %>% 
-#   dplyr::filter() 
-
-# ----- save-to-disk ---------------------------------------------------------------
-
-# ds_gls                   %>% readr::write_csv("./data-public/derived/gls.csv")
-# ds_population            %>% readr::write_csv("./data-public/derived/population.csv")
-# ds_suicide               %>% readr::write_csv("./data-public/derived/suicide.csv")
-ds_aggregate_2          %>% readr::write_csv("./data-public/derived/aggregate-2-gls.csv")
