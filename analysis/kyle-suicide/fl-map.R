046a41072a1487921f4012ea4978bf445ca3292a
Andriy V. Koval <koval.andrey@gmail.com>
2020-04-08 18:04:38 -0400

    on call with Kyle

--- analysis/kyle-suicide/fl-map.R
+++ analysis/kyle-suicide/fl-map.R
@@ -44,6 +44,7 @@ youth_age_group <-   lvl_age_groups[4:6]
 ds_population_suicide_raw<-   readr::read_rds(path_file_input)
 
 # ---- load-map-data -----------------------------------------------------------
+# must be in the environement for function to operate
 florida_counties_map <- map_data("county") %>% 
   dplyr::filter(region == "florida") %>% 
   dplyr::mutate_at(
@@ -104,7 +105,7 @@ ds_youth_county_race_totals <- ds_youth0 %>%
 # ---- base-map ---------------------------------------------------------------
 
 #base map - no other data added
-
+# ne
 county_map <- florida_counties_map %>% 
   ggplot(aes(x = long, y = lat, group = group)) +
   geom_polygon(fill = "white", color = "black") +
@@ -114,29 +115,68 @@ county_map
 
 
 # ---- total-youth-suicides ----------------------------------------------------
+# function to compute the dataset
+prep_data_map <- function(d, group_by_var, d_map = florida_counties_map ){
+  # d <- ds_youth0
+  # group_by_var <- c("county","year")
+  # d_map <- florida_counties_map
 
-youth_total_map_data <- florida_counties_map %>% 
-  dplyr::left_join(ds_youth_county_totals, by = c("subregion" = "county"))
+  d <- ds_youth0 %>% 
+    dplyr::group_by(.dots = group_by_var) %>% 
+    dplyr::summarise(
+      n_population = sum(n_population, na.rm = TRUE)
+      ,n_suicides   = sum(n_suicides, na.rm = TRUE)
+    ) %>%
+    dplyr::ungroup() %>% 
+    dplyr::mutate(
+      rate_per100k_suicide  = (( n_suicides/n_population)*100000)
+    ) %>% 
+    dplyr::right_join(d_map, by = c( "county" = "subregion"))
+  d
+  return(d)
+}
+# How to use
+# d <- ds_youth0 %>% prep_data_map(c("county","year","sex"))
+d <- ds_youth0 %>% prep_data_map(c("county","year"))
 
-g_youth_total_map <-  youth_total_map_data %>% 
-  dplyr::filter(year == 2017) %>% 
-  ggplot(aes(x = long, y = lat, group = group, fill = n_suicides)) +
-  geom_polygon(color = "black") +
-  coord_map() +
-  theme_void() +
-  scale_fill_gradient2(
-    low   = "#fcfbfd" 
-    ,mid  = "#9e9ac8"
-    ,high = "#3f007d"
-    ,midpoint = median(youth_total_map_data$n_suicides)
-  ) +
-  labs(
-    title = "Total Youth (10-24) Suicides in Florida for 2017"
-    ,fill = "Number of Suicides"
-  )
-g_youth_total_map  
+# function to plot the map
+plot_florida_suicides <- function(d, year_i, metric){
+    # d <- youth_total_map_data
+  # year_i <- 2017
+  # metric <- "n_suicides"
+  # 
+  g1 <-  d %>% 
+    dplyr::filter(year == year_i) %>%
+    ggplot(aes_string(x = "long", y = "lat", group = "group", fill = metric)) +
+    geom_polygon(color = "black") +
+    coord_map() +
+    theme_void() +
+    # scale_fill_continuous(label = scales::label_comma())+
+    scale_fill_gradient2(
+      low   = "#fcfbfd" 
+      ,mid  = "#9e9ac8"
+      ,high = "#3f007d"
+      ,midpoint = median(youth_total_map_data$n_suicides)
+      ,label = scales::label_comma()
+    ) +
+    theme(
+      legend.position = c(.4, .5)
+    )+
+    # facet_wrap(~year)+
+    labs(
+      title = paste0("Total Youth (10-24) Suicides in Florida for ",year_i)
+      ,fill = "Number of Suicides"
+    )
+  # g1
+  return(g1)
+}
+# How to use
+# youth_total_map_data %>% plot_florida_suicides(2016,"n_suicides")
+youth_total_map_data %>% plot_florida_suicides(2016,"n_population")
 
-
+ds_youth0 %>% 
+  prep_data_map(c("county","year")) %>% 
+  plot_florida_suicides(2008,"rate_per100k_suicide")
 
 # ---- youth suicides by Race + Ethnicity --------------------------------------
 
